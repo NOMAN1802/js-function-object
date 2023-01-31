02be966ff53ed0e16853c7cb7d2632e659230cea
NOMAN1802 <mustakimnoman82@gmail.com>
2023-01-31 22:27:30 +0600

    function return

---
+++ function.js
@@ -0,0 +1,13 @@
+// decleare a function 
+function startFan(){
+    console.log('Stand up');
+    console.log('walk toward the switch');
+    console.log('Press the switch');
+}
+// call the function 
+startFan();
+console.log('waking up in the morning');
+startFan();
+console.log('Eating lunch');
+console.log('Watching JS tutorial');
+startFan();
